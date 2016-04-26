c4d06fdc6130e7665303e9e5943405c716a5f114
Fredrik Ekre <fredrik.ekre@hotmail.com>
2016-04-26 09:34:21 +0200

    restructure

--- reference_solution/2D/src/vtkwriter.jl
+++ reference_solution/2D/src/vtkwriter.jl
@@ -23,9 +23,7 @@ function vtkwriter(pvd,i,u_mesh,u,d,Ψ,σe)
     displacement = reshape(u,(2,nnodes))
     displacement = [displacement; zeros(nnodes)']
 
-    dof = reshape(1:2*nnodes,(2,nnodes))
-
-    vtkfile = vtk_grid_no_compress(u_mesh.mesh,u_mesh.coord,"./vtkfiles/step_$i")
+    vtkfile = vtk_grid_no_compress(u_mesh.mesh,u_mesh.coord,"./vtkfiles_damage/step_$i")
     vtk_point_data(vtkfile,displacement,"displacement")
     vtk_point_data(vtkfile,d,"damage")
     vtk_cell_data(vtkfile, Ψ, "energy")
@@ -41,11 +39,22 @@ function vtkwriter(pvd,i,u_mesh,u,d)
     displacement = reshape(u,(2,nnodes))
     displacement = [displacement; zeros(nnodes)']
 
-    dof = reshape(1:2*nnodes,(2,nnodes))
-
-    vtkfile = vtk_grid_no_compress(u_mesh.mesh,u_mesh.coord,"./vtkfiles/step_$i")
+    vtkfile = vtk_grid_no_compress(u_mesh.mesh,u_mesh.coord,"./vtkfiles_damage/step_$i")
     vtk_point_data(vtkfile,displacement,"displacement")
     vtk_point_data(vtkfile,d,"damage")
+    collection_add_timestep(pvd,vtkfile,float(i))
+
+end
+
+function vtkwriter(pvd,i,u_mesh,u)
+
+    nnodes = div(length(u),2)
+
+    displacement = reshape(u,(2,nnodes))
+    displacement = [displacement; zeros(nnodes)']
+
+    vtkfile = vtk_grid_no_compress(u_mesh.mesh,u_mesh.coord,"./vtkfiles_elastic/step_$i")
+    vtk_point_data(vtkfile,displacement,"displacement")
     collection_add_timestep(pvd,vtkfile,float(i))
 
 end
